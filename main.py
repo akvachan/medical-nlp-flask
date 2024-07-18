5d0f085b8a2b4a6c850a05f363afd863f054657e
Arsenii Kvachan <155106737+akvachan@users.noreply.github.com>
2024-07-18 10:03:03 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import tf_model
+import os
+
+model_url = 'https://drive.google.com/uc?export=download&id=1PUVyn7eSzAmtMF36mNHRPRFA0JVgAWY2'
+model_path = 'poseidon_lstm'
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        text = request.form['abstractText']
+        sentences = tf_model.sentences(text)
+        model = tf_model.load_tf_model(model_path)
+        predictions = tf_model.predict(sentences, model)
+        struct_sents = tf_model.structure_sentences(sentences, predictions)
+
+        return render_template('index.html', segmented_abstract=struct_sents, original_text=text)
+    else:
+        return render_template('index.html', segmented_abstract=None, original_text="")
+
+if __name__ == '__main__':
+    tf_model.download_and_extract_model(model_url, model_path)
+    app.run(port=os.getenv("PORT", default=5000), debug=True)
